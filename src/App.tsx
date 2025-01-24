cffbf63483ec06cc2f918d260e72913b49de5215
Scot Darcy <scotdarcy@Scots-MBP-4-2.broadband>
2025-01-24 11:32:47 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const Application = () => {
+  return (
+    <main className="flex flex-col gap-8">
+      <h1>Testing Items</h1>
+    </main>
+  );
+};
+
+export default Application;
---
+++ vitest.config.ts
@@ -0,0 +1,37 @@
+import path from 'node:path';
+import { defineConfig, defaultExclude } from 'vitest/config';
+import configuration from './vite.config';
+
+export default defineConfig({
+  ...configuration,
+  resolve: {
+    alias: {
+      ...configuration?.resolve?.alias,
+      test: path.resolve(__dirname, './test'),
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: path.resolve(__dirname, 'src/test/setup.ts'),
+    exclude: [...defaultExclude, '**/*.svelte**'],
+    environmentMatchGlobs: [
+      ['**/*.test.tsx', 'jsdom'],
+      ['**/*.component.test.ts', 'jsdom'],
+    ],
+    coverage: {
+        provider:'v8',
+      include: ['src/**/*'],
+      exclude: [
+        'test/**',
+        'vite.*.ts',
+        '**/*.d.ts',
+        '**/*.test.*',
+        '**/*.config.*',
+        '**/snapshot-tests/**',
+        '**/*.solution.tsx',
+        '**/coverage/**',
+      ],
+      all: true,
+    },
+  },
+});
